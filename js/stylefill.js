e0ed45584623ea13bc94091aea811e1f069eed89
Nathan Ford <nathan@markboultondesign.com>
2013-12-06 14:43:29 +0000

    Amending feature detection for IE lte 9

--- js/stylefill.js
+++ js/stylefill.js
@@ -43,7 +43,7 @@ var stylefill = {
 	
 	getStyleSheets : function () {
 	
-		if (Array.prototype.slice.call(document.documentElement)) { // feature detect for IE lte 9
+		try { // feature detect for IE lte 9
 	
 			var sheetstext = new Array(),
 					sheets = Array.prototype.slice.call(document.getElementsByTagName('link')); // grab stylesheet links - not used yet
@@ -64,7 +64,7 @@ var stylefill = {
 			return sheetstext;
 		
 		}
-		else return false;
+		catch (e) { return false; }
 	
 	},
 	
